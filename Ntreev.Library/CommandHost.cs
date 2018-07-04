b102171846968c866c1bfc83abff03447205efdc
s2quake <s2quake@ntreev.com>
2018-07-04 18:05:58 +0900

    콘솔창엥서 명령어를 호출하기 위햇 명령어를 조합해 직접 실행할 수 있는 기능을 추가

---
+++ Ntreev.Library/CommandHost.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ntreev.Library
+{
+    public class CommandHost : IEnumerable<object>
+    {
+        private readonly string filename;
+        private readonly string workingPath;
+        private readonly string commandName;
+        private readonly List<object> items = new List<object>();
+
+        public CommandHost(string filename, string workingPath, string commandName)
+        {
+            this.filename = filename;
+            this.workingPath = workingPath;
+            this.commandName = commandName;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.filename} {this.commandName} {string.Join(" ", this.items)}";
+        }
+
+        public void Add(object item)
+        {
+            this.items.Add(item);
+        }
+
+        public string Run()
+        {
+            var sb = new StringBuilder();
+            var process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = this.filename;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.WorkingDirectory = this.workingPath;
+            process.StartInfo.Arguments = $"{this.commandName} {string.Join(" ", this.items)}";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+            process.OutputDataReceived += (s, e) =>
+            {
+                sb.AppendLine(e.Data);
+            };
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+                throw new Exception(sb.ToString());
+
+            return sb.ToString();
+        }
+
+        public string ReadLine()
+        {
+            var lines = this.ReadLines(true);
+            return lines.Single();
+        }
+
+        public string[] ReadLines()
+        {
+            return this.ReadLines(false);
+        }
+
+        public string[] ReadLines(bool removeEmptyLine)
+        {
+            var lines = this.Run();
+            return this.GetLines(lines, removeEmptyLine);
+        }
+
+        private string[] GetLines(string text, bool removeEmptyLine)
+        {
+            using (var sr = new StringReader(text))
+            {
+                var line = null as string;
+                var lineList = new List<string>();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Trim() != string.Empty || removeEmptyLine == false)
+                    {
+                        lineList.Add(line);
+                    }
+                }
+                return lineList.ToArray();
+            }
+        }
+
+        #region IEnumerable
+
+        IEnumerator<object> IEnumerable<object>.GetEnumerator()
+        {
+            foreach (var item in this.items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (var item in this.items)
+            {
+                yield return item;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Ntreev.Library/CommandOption.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ntreev.Library
+{
+    public class CommandOption
+    {
+        private object key;
+        private object value;
+
+        public CommandOption(string name)
+        {
+            this.key = name;
+        }
+
+        public CommandOption(char name)
+        {
+            this.key = name;
+        }
+
+        public CommandOption(string name, object value)
+        {
+            this.key = name;
+            this.value = value;
+        }
+
+        public CommandOption(char name, object value)
+        {
+            this.key = name;
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            if (this.key is string s)
+            {
+                if (this.value == null)
+                    return $"--{this.key}";
+                else
+                    return $"--{this.key} {this.value}";
+            }
+            else if (this.key is char c)
+            {
+                if (this.value == null)
+                    return $"-{this.key}";
+                else
+                    return $"-{this.key} {this.value}";
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
