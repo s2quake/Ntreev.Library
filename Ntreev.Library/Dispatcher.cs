a8479e167e634a808af19d8d03e557699934316e
s2quake <han0210@netsgo.com>
2018-10-21 19:23:10 +0900

    디스패처 추가

---
+++ Ntreev.Library/Dispatcher.cs
@@ -0,0 +1,133 @@
+﻿//Released under the MIT License.
+//
+//Copyright (c) 2018 Ntreev Soft co., Ltd.
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+//documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
+//rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+//persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the 
+//Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+//WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+//COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+//OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#pragma warning disable 0612
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ntreev.Library
+{
+    public class Dispatcher
+    {
+        private readonly DispatcherScheduler scheduler;
+        private readonly TaskFactory factory;
+        private readonly CancellationTokenSource cancellationToken;
+
+        public Dispatcher(object owner)
+        {
+            this.cancellationToken = new CancellationTokenSource();
+            this.scheduler = new DispatcherScheduler(this.cancellationToken.Token);
+            this.factory = new TaskFactory(new CancellationToken(false), TaskCreationOptions.None, TaskContinuationOptions.None, this.scheduler);
+            this.Owner = owner;
+            this.Thread = new Thread(() =>
+            {
+                this.scheduler.Run();
+                this.OnDisposed(EventArgs.Empty);
+            })
+            {
+                Name = owner.ToString()
+            };
+            this.Thread.Start();
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Owner}";
+        }
+
+        public void VerifyAccess()
+        {
+            if (!this.CheckAccess())
+            {
+                throw new InvalidOperationException("The calling thread cannot access this object because a different thread owns it.");
+            }
+        }
+
+        public bool CheckAccess()
+        {
+            return this.Thread == Thread.CurrentThread;
+        }
+
+        public void Invoke(Action action)
+        {
+            if (this.CheckAccess() == true)
+            {
+                action();
+            }
+            else
+            {
+                var task = this.factory.StartNew(action);
+                task.Wait();
+            }
+        }
+
+        public Task InvokeAsync(Action action)
+        {
+            return this.factory.StartNew(action);
+        }
+
+        public TResult Invoke<TResult>(Func<TResult> callback)
+        {
+            if (this.CheckAccess() == true)
+            {
+                return callback();
+            }
+            else
+            {
+                var task = this.factory.StartNew(callback);
+                task.Wait();
+                return task.Result;
+            }
+        }
+
+        public Task<TResult> InvokeAsync<TResult>(Func<TResult> callback)
+        {
+            return this.factory.StartNew(callback);
+        }
+
+        public void Dispose()
+        {
+            this.cancellationToken.Cancel();
+            this.scheduler.Continue();
+        }
+
+        public async Task DisposeAsync()
+        {
+            var task = this.factory.StartNew(() => { });
+            this.cancellationToken.Cancel();
+            this.scheduler.Continue();
+            await task;
+        }
+
+        public string Name => this.Owner.ToString();
+
+        public object Owner { get; }
+
+        public Thread Thread { get; }
+
+        public event EventHandler Disposed;
+
+        protected virtual void OnDisposed(EventArgs e)
+        {
+            this.Disposed?.Invoke(this, e);
+        }
+    }
+}
---
+++ Ntreev.Library/DispatcherScheduler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ntreev.Library
+{
+    class DispatcherScheduler : TaskScheduler
+    {
+        private static readonly object lockobj = new object();
+        private readonly CancellationToken cancellation;
+        private readonly BlockingCollection<Task> taskQueue = new BlockingCollection<Task>();
+        private ManualResetEvent eventSet = new ManualResetEvent(false);
+        private bool isExecuting;
+
+        public DispatcherScheduler(CancellationToken cancellation)
+        {
+            this.cancellation = cancellation;
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            return null;
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            this.taskQueue.Add(task, this.cancellation);
+            this.eventSet.Set();
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            if (taskWasPreviouslyQueued)
+                return false;
+
+            return this.isExecuting && TryExecuteTask(task);
+        }
+
+        internal void Continue()
+        {
+            this.eventSet.Set();
+        }
+
+        internal void Run()
+        {
+            while (true)
+            {
+                if (this.taskQueue.TryTake(out var task) == true)
+                {
+                    this.isExecuting = true;
+                    this.TryExecuteTask(task);
+                    this.isExecuting = false;
+                    this.eventSet.Set();
+                }
+                else if (this.cancellation.IsCancellationRequested == true)
+                {
+                    break;
+                }
+                else
+                {
+                    eventSet.WaitOne();
+                    eventSet.Reset();
+                }
+            }
+        }
+    }
+}
