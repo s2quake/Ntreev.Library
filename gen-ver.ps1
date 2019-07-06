b966a6a486ef60afefdaa63436189a039e48e0d9
s2quake <han0210@netsgo.com>
2019-07-06 13:00:58 +0900

    gen_ver.ps1 수정 사용하지 않는 경로 삭제

--- gen-ver.ps1
+++ gen-ver.ps1
@@ -1,84 +1,22 @@
 $majorVersion = 4
 $minorVersion = 0
-$assemblyFilePath = ".\Ntreev.Library.AssemblyInfo\AssemblyInfo.cs"
-$projectFilePath = ".\Ntreev.Library\Ntreev.Library.csproj"
-
-$assemblyPath = Join-Path (Split-Path $myInvocation.MyCommand.Definition) $assemblyFilePath -Resolve
-$projectPath = Join-Path (Split-Path $myInvocation.MyCommand.Definition) $projectFilePath
 $version = "$majorVersion.$minorVersion"
 $fileVersion = "$majorVersion.$minorVersion" + "." + (Get-Date -Format yy) + (Get-Date).DayOfYear + "." + (Get-Date -Format HHmm)
 
-if (Test-Path $assemblyPath) {
-    $content = Get-Content $assemblyPath -Encoding UTF8
-
-    $pattern1 = "(AssemblyVersion[(]`").+(`"[)]])"
-    if ($content -match $pattern1) {
-        $content = $content -replace $pattern1, "`${1}$version`$2"
-    }
-
-    $pattern2 = "(AssemblyFileVersion[(]`").+(`"[)]])"
-    if ($content -match $pattern2) {
-        $content = $content -replace $pattern2, "`${1}$fileVersion`$2"
-    }
-
-    $pattern3 = "(AssemblyInformationalVersion[(]`").+(`"[)]])"
-    if ($content -match $pattern3) {
-        $content = $content -replace $pattern3, "`${1}$fileVersion`$2"
-    }
-
-    $backupPath = $assemblyPath + ".bak"
-    Copy-Item $assemblyPath $backupPath
-    Set-Content $assemblyPath $content -Encoding UTF8
-
-    if ($null -eq (Get-Content $assemblyPath)) {
-        Remove-Item $assemblyPath
-        Copy-Item $backupPath $assemblyPath
-        Remove-Item $backupPath
-        throw "replace version failed: $assemblyPath"
-    }
-    else {
-        Remove-Item $backupPath
+Get-ChildItem -Path $PSScriptRoot -Filter "*.csproj" -Recurse | ForEach-Object {
+    [xml]$doc = Get-Content $_.FullName
+    if ($doc.Project.Sdk -eq "Microsoft.NET.Sdk") {
+        if ($doc.Project.PropertyGroup.Version) {
+            $doc.Project.PropertyGroup.Version = $fileVersion
+        }
+        if ($doc.Project.PropertyGroup.FileVersion) {
+            $doc.Project.PropertyGroup.FileVersion = $fileVersion
+        }
+        if ($doc.Project.PropertyGroup.AssemblyVersion) {
+            $doc.Project.PropertyGroup.AssemblyVersion = $version
+        }
+        $doc.Save($_.FullName)
     }
 }
-else {
-    throw "assembly path not found: $assemblyPath"
-}
 
-if (Test-Path $projectPath) {
-    $content = Get-Content $projectPath -Encoding UTF8
-
-    $pattern1 = "(<Version>)(.*)(</Version>)"
-    if ($content -match $pattern1) {
-        $content = $content -replace $pattern1, "`${1}$fileVersion`$3"
-    }
-
-    $pattern2 = "(<FileVersion>)(.*)(</FileVersion>)"
-    if ($content -match $pattern2) {
-        $content = $content -replace $pattern2, "`${1}$fileVersion`$3"
-    }
-
-    $pattern3 = "(<AssemblyVersion>)(.*)(</AssemblyVersion>)"
-    if ($content -match $pattern3) {
-        $content = $content -replace $pattern3, "`${1}$version`$3"
-    }
-
-    $backupPath = $projectPath + ".bak"
-    Copy-Item $projectPath $backupPath
-    Set-Content $projectPath $content -Encoding UTF8
-
-    if ($null -eq (Get-Content $projectPath)) {
-        Remove-Item $projectPath
-        Copy-Item $backupPath $projectPath
-        Remove-Item $backupPath
-        throw "replace version failed: $projectPath"
-    }
-    else {
-        Remove-Item $backupPath
-    }
-}
-else {
-    throw "project path not found: $projectPath"
-}
-
-Set-Content version.txt $fileVersion
 Write-Host $fileVersion
