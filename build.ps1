9ea0793681fd07ce7214640f02ac223d57a3ec2e
s2quake <han0210@netsgo.com>
2020-08-15 22:20:58 +0900

    빌드 스크립트 작성중

--- build.ps1
+++ build.ps1
@@ -3,47 +3,7 @@ $minorVersion = 0
 $buildVersion = 0
 $revision = "unversioned"
 $propsPath = "$PSScriptRoot\base.props"
-
-$version = "$majorVersion.$minorVersion.$buildVersion-`$(TargetFramework)-$revision"
-$assemblyVersion = "$majorVersion.$minorVersion"
-$fileVersion = "$majorVersion.$minorVersion.$buildVersion"
-
-function Confirm-Command {
-    param (
-        [string]$CommandName,
-        [string]$ErrorMessage
-    )
-    try {
-        Invoke-Expression -Command $CommandName | Out-Null
-        Write-Host "$CommandName ..OK"
-    }
-    catch {
-        Write-Error $_.Exception.Message
-        Write-Host "Please visit the site below and install it."
-        Write-Host "https://dotnet.microsoft.com/download/dotnet-core/$needVersion"
-        Write-Host 'Press any key to continue...';
-        $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-        exit 1
-    }
-}
-
-# Confirm-Command "dotnet"
-# Confirm-Command "git"
-
-# function Get-Framework40-Family-Version() {
-#     $result = -1
-#     if (Is-Key-Present "HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Client" "Install" -or Is-Key-Present "HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Full" "Install") {
-#         # .net 4.0 is installed
-#         $result = 0
-#         $version = Get-Framework-Value "HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Full" "Release"
-        
-#         if ($version -ge 378389) {
-#             # .net 4.5
-#             Write-Host "Installed .Net Framework 4.5"
-#             $result = 1
-#         }   
-#     }
-# }
+$frameworkOption = ""
 
 # validate dotnet version
 try {
@@ -62,6 +22,43 @@ catch {
     exit 1
 }
 
+# check if .netframework 4.5 is installed on windows
+try {
+    if ([environment]::OSVersion.Platform -eq "Win32NT") {
+        function Test-KeyPresent([string]$path, [string]$key) {
+            if (!(Test-Path $path)) { return $false }
+            if ($null -eq (Get-ItemProperty $path).$key) { return $false }
+            return $true
+        }
+        function Get-Framework-Value([string]$path, [string]$key) {
+            if (!(Test-Path $path)) { return "-1" }
+        
+            return (Get-ItemProperty $path).$key  
+        }
+        if (Test-KeyPresent "HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Client" "Install" -or Test-KeyPresent "HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Full" "Install") {
+            $version = Get-Framework-Value "HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Full" "Release"
+            if ($version -lt 378389) {
+                throw ".Net Framework 4.5 not installed."
+            }   
+        }    
+    }
+}
+catch {
+    Write-Warning $_.Exception.Message
+}
+
+# check if there are any changes in the repository.
+try {
+    $changes = Invoke-Expression "git status --porcelain"
+    if ($changes) {
+        throw "git repository has changes. build aborted."
+    }
+}
+catch {
+    Write-Error $_.Exception.Message
+    exit 1
+}
+
 # get head revision of this repository
 try {
     $revision = Invoke-Expression -Command "git rev-parse head" 2>&1 -ErrorVariable errout
@@ -74,18 +71,10 @@ catch {
     Write-Warning "revision is '$revision'"
 }
 
-try {
-    $changes = Invoke-Expression "git status --porcelain"
-    if ($changes) {
-        throw "git repository has changes. build aborted."
-    }
-}
-catch {
-    Write-Error $_.Exception.Message
-    exit 1
-}
-
 # recored version
+$version = "$majorVersion.$minorVersion.$buildVersion-`$(TargetFramework)-$revision"
+$assemblyVersion = "$majorVersion.$minorVersion"
+$fileVersion = "$majorVersion.$minorVersion.$buildVersion"
 [xml]$doc = Get-Content $propsPath -Encoding UTF8
 foreach ($obj in $doc.Project.PropertyGroup) {        
     if ($obj.Version) {
@@ -100,11 +89,13 @@ foreach ($obj in $doc.Project.PropertyGroup) {
 }
 $doc.Save($propsPath)
 
-Invoke-Expression "dotnet restore"
-Invoke-Expression "dotnet clean"
-Invoke-Expression "dotnet build"
+# build project
+Invoke-Expression "dotnet build $frameworkOption --verbosity minimal --nologo"
+
+# revert file
 Invoke-Expression "git checkout $propsPath"
 
+# print version
 Write-Host "AssemblyVersion: $assemblyVersion"
 Write-Host "FileVersion: $fileVersion"
 Write-Host "Version: $version"
