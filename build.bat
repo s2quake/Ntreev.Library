4656811caab1c65b4f9ef61289c494530178157a
s2quake <han0210@netsgo.com>
2020-08-16 20:20:55 +0900

    빌드 스크립트 웹에서 받아 실행하는것 다시 테스트

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -executionpolicy remotesigned -File %~dp0\build.ps1 -BuildPath %~dp0 -PropsPath "base.props" -Force
+powershell -executionpolicy remotesigned -File %~dp0\build.ps1
