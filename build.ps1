047f31d46da4e4ed1f2f54ca2964f3ea35150e3e
s2quake <han0210@netsgo.com>
2020-08-16 20:26:48 +0900

    빌드시 -Force 인자 제거

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ $location = Get-Location
 try {
     Set-Location $PSScriptRoot
     Invoke-WebRequest -Uri "https://gist.githubusercontent.com/s2quake/57ae08b7598f1f4978d8c50326b4086d/raw/dca3009fc111b25897f18688c94ece6a74e777bb/build.ps1" -OutFile .\.vscode\build.ps1
-    .\.vscode\build.ps1 -BuildPath $PSScriptRoot -PropsPath "base.props" -Force
+    .\.vscode\build.ps1 -BuildPath $PSScriptRoot -PropsPath "base.props"
 }
 finally {
     Set-Location $location
